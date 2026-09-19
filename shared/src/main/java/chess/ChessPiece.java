56a4907684bfd3d1e774d0a330d5187fb370886d
esplinsam <esplinsam@gmail.com>
2026-09-19 16:19:05 -0600

    impl(0): Successfully passed BishopMoves test cases

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.Collection;
 import java.util.Objects;
+import java.util.ArrayList;
 
 /**
  * Represents a single chess piece
@@ -49,6 +50,92 @@ public class ChessPiece {
     }
 
     /**
+     * Helper method for adding bishop moves
+     */
+    public void addBishopMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves) {
+        int [][] directions = {
+                {1, 1},
+                {1, -1},
+                {-1, 1},
+                {-1, -1}
+        };
+
+        for (int[] direction: directions) {
+            int row = myPosition.getRow() + direction[0];
+            int col = myPosition.getColumn() + direction[1];
+
+            while (row >= 1 && col >= 1
+                    && row <= 8 && col <= 8) {
+                // Define the target position and the piece occupying that square
+                ChessPosition endPosition = new ChessPosition(row, col);
+                ChessPiece piece = board.getPiece(endPosition);
+
+                // Define the move to make
+                ChessMove move = new ChessMove(myPosition, endPosition, null);
+
+                if (piece == null) { // simply add the move if the square is empty
+                    moves.add(move);
+                }
+                else if (this.pieceColor != piece.getTeamColor()) { // if square is occupied by opposing piece add move and stop looking in that direction
+                    moves.add(move);
+                    break;
+                }
+                else { // if square is occupied by piece of same color stop looking in that direction
+                    break;
+                }
+
+                // move to the next square in that direction
+                row += direction[0];
+                col += direction[1];
+
+            }
+        }
+    }
+
+    /**
+     * Helper method for adding bishop moves
+     */
+    public void addRookMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves) {
+        int [][] directions = {
+                {1, 1},
+                {1, -1},
+                {-1, 1},
+                {-1, -1}
+        };
+
+        for (int[] direction: directions) {
+            int row = myPosition.getRow() + direction[0];
+            int col = myPosition.getColumn() + direction[1];
+
+            while (row >= 1 && col >= 1
+                    && row <= 8 && col <= 8) {
+                // Define the target position and the piece occupying that square
+                ChessPosition endPosition = new ChessPosition(row, col);
+                ChessPiece piece = board.getPiece(endPosition);
+
+                // Define the move to make
+                ChessMove move = new ChessMove(myPosition, endPosition, null);
+
+                if (piece == null) { // simply add the move if the square is empty
+                    moves.add(move);
+                }
+                else if (this.pieceColor != piece.getTeamColor()) { // if square is occupied by opposing piece add move and stop looking in that direction
+                    moves.add(move);
+                    break;
+                }
+                else { // if square is occupied by piece of same color stop looking in that direction
+                    break;
+                }
+
+                // move to the next square in that direction
+                row += direction[0];
+                col += direction[1];
+
+            }
+        }
+    }
+
+    /**
      * Calculates all the positions a chess piece can move to
      * Does not take into account moves that are illegal due to leaving the king in
      * danger
@@ -58,8 +145,32 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         // Use specifics of ChessPiece.PieceType to determine which squares a piece can move to
         // Check possible squares to move to, to see if a piece is there already.
-        return null;
+        Collection<ChessMove> moves = new ArrayList<>();
+
+        // implement here
+        switch (this.getPieceType()) {
+            case BISHOP ->
+                addBishopMoves(board, myPosition, moves);
+            case ROOK -> {
+                // Implement rook's moveset here
+            }
+            case KNIGHT -> {
+                // Implement horsie's moveset here
+            }
+            case QUEEN -> {
+                // Implement queen's moveset here
+            }
+            case KING -> {
+                // Implement king's moveset here
+            }
+            case PAWN -> {
+                // Implement pawn's moveset here
+            }
+        }
+
+        return moves;
     }
+
     @Override
     public boolean equals(Object object) {
         if (this == object) {
